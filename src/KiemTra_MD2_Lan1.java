0d6ec5c465aaa32a535fbcda61485f48ae4b15c3
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-05 15:45:26 +0700

    KiemTra

---
+++ src/KiemTra_MD2_Lan1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class KiemTra_MD2_Lan1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int []arr={};
+        System.out.println("Nhap vao so phan tu cua mang");
+    }
+}
