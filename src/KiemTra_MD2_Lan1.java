37b1d4f38a53511ab4450b4d85dc0cdc7cca83b9
Trần Quang Huy <tranquanghuy@MacBook-Pro-cua-Tran.local>
2022-04-05 16:30:33 +0700

    KiemTra

--- src/KiemTra_MD2_Lan1.java
+++ src/KiemTra_MD2_Lan1.java
@@ -7,12 +7,13 @@ public class KiemTra_MD2_Lan1 {
         System.out.println("Nhập số lượng phần tử mảng");
         size = scanner.nextInt();
         int[] mang = new int[size];
+
         //bai 3
+
         int sum = 0;
-        sum = scanner.nextInt();
         for (int i = 0; i < size; i++) {
             if (mang[i] % 2 == 0) {
-                sum = sum + mang[i];
+                sum = (int) (sum + mang[i]);
             }
         }
         System.out.println("tổng chẵn các phần tử trong mảng: " + sum);
@@ -20,9 +21,9 @@ public class KiemTra_MD2_Lan1 {
         System.out.println("Nhập a: ");
         int a = scanner.nextInt();
         for (int j = 0; j < mang.length; j++) {
-if(mang[j] == a){
-    System.out.println("vi tri cua a trong mang là: "+ j);
-}
+            if (mang[j] == a) {
+                System.out.println("vi tri cua a trong mang là: " + j);
+            }
         }
 
     }
